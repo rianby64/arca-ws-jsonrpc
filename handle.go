482f3748ecf0e466bb3edfb30d0d4fa9f2c3d2f5
Andres Rios <Andres_Rios@epam.com>
2018-12-18 21:32:54 +0300

    Ok...

--- handle.go
+++ handle.go
@@ -8,15 +8,15 @@ import (
 func (s *JSONRPCServerWS) Handle(
 	w http.ResponseWriter,
 	r *http.Request,
-) error {
+) {
 	if s.connections == nil ||
 		s.tick == nil {
 		s.Init()
 	}
 	conn, err := s.transport.upgradeConnection(w, r)
 	if err != nil {
-		return err
+		return
 	}
 
-	return s.listenAndResponse(conn)
+	s.listenAndResponse(conn)
 }
